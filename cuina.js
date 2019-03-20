0459bed5f5abe2855d509e6d7ceaa7a8b1502aad
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-03-20 20:24:17 +0100

    algo

--- cuina.js
+++ cuina.js
@@ -6,25 +6,23 @@ var express = require('express');
 	app = express();
 
 	app.get ('/receptes/llista', function(req, res){
-
 		//var dbo = res.db("cocina");
 		//var consulta = dbo.getCollection('recetas').find({});
 		MongoClient.connect(url, function(err, db) {
 		  if (err) throw err;
 		  var dbo = db.db("cocina");
 
-		  dbo.collection("cocina").find({}).toArray(function(err, result) {
+		  dbo.collection("recetas").find({}).toArray(function(err, result) {
 		    if (err) throw err;
+		   		console.log(result);
+		    	res.send(result);
+		    	/*
 		    result.forEach(function(receta){
-		    console.log(result);
-		    res.send(result);	
 		    });
-
+		    */
 		    db.close();
 		  });
-
-		});
-		
+		});	
 	});
 
 	app.use(function(req, res){
