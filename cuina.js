13e206aab58c4cd73ef00ddb9033d20e330a3aca
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-03-28 20:26:23 +0100

    ahora recibo datos

--- cuina.js
+++ cuina.js
@@ -2,6 +2,9 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var $ = require('jquery');
 fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 var express = require('express');
 	app = express();
@@ -37,39 +40,54 @@ var express = require('express');
 		  });
 		});	
 	});
+	var formulario = '<form method="POST" action="/crear">\
+						<label for="nombre">Nombre</label>\
+						<input type="text" name="nombre" id="edad">\
+						<br>\
+						<label for="duracion">Duracion</label>\
+						<input type="text" name="duracion">\
+						<br>\
+						<label for="descripcion">Descripcion</label>\
+						<input type="text" name="descripcion">\
+						<br>\
+						<label for="tipo">Tipo</label>\
+						<input type="text" name="tipo">\
+						<br>\
+						<input type="submit">\
+					</form>';
 
 	app.get ('/crear', function(req, res){
 		//var dbo = res.db("cocina");
 		//var consulta = dbo.getCollection('recetas').find({});
+		/*
 		MongoClient.connect(url, function(err, db) {
 		  if (err) throw err;
 		  var dbo = db.db("cocina");
-
-			var formulario = '<form method="POST" action="/crear">\
-				<label for="nombre">Nombre</label>\
-				<input type="text" name="nombre">\
-				<br>\
-				<label for="duracion">Duracion</label>\
-				<input type="text" name="duracion">\
-				<br>\
-				<label for="descripcion">Descripcion</label>\
-				<input type="text" name="descripcion">\
-				<br>\
-				<label for="tipo">Tipo</label>\
-				<input type="text" name="tipo">\
-				<br>\
-				<input type="submit">\
-			</form>';
-			
-
-		  res.send(formulario);
-
+			*/
+		  res.send('<html><body>'
+			  +formulario
+			  + '</body></html>');
+/*
 		});	
+*/
 	});
 
 	app.post ('/crear', function(req, res){
 		//var dbo = res.db("cocina");
 		//var consulta = dbo.getCollection('recetas').find({});
+
+		var nombre = req;
+		console.log(nombre);
+		//var duracion = req.body.duracion || '';
+		//var descripcion = req.body.descripcion || '';
+		//var tipo = req.body.tipo || '';
+	
+		res.send('<html><body>'
+			+ req
+			+ formulario
+			+ '</html></body>'
+		);
+		/*
 		MongoClient.connect(url, function(err, db) {
 		  if (err) throw err;
 		  var dbo = db.db("cocina");
@@ -80,7 +98,46 @@ var express = require('express');
 		  res.send(formulario);
 
 		});	
+		*/
 	});
+
+	var formulario = '<form method="post" action="/nacimiento">'
+    + '<label for="edad">¿Qué edad tienes?</label>'
+    + '<input type="text" name="edad" id="edad">'    
+    + '<input type="submit" value="Enviar"/>'
+    + '</form>';
+ 
+var cabecera = '<h1>Naciste el año</h1>';
+ 
+app.get('/nacimiento', function (req, res) {
+ 
+    res.send('<html><body>'
+            + cabecera
+            + formulario
+            + '</html></body>'
+    );
+ 
+});
+
+app.post('/nacimiento',urlencodedParser, function (req, res) {
+ 
+    var edad = req.body.edad || '';
+    var nacimiento = '';
+ 
+    if (edad != '')
+        nacimiento = 2015 - edad;
+ 
+    res.send('<html><body>'
+            + cabecera
+            + '<p>' + nacimiento + '</p>'
+            + formulario
+            + '</html></body>'
+    );
+ 
+});
+
+
+
 
 	app.use(function(req, res){
 
