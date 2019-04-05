c766b106c84662e231086d5f4f82484aecab0da6
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-04-05 20:24:32 +0200

    falta filtrar

--- cuina.js
+++ cuina.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
+var fs = require('fs');
 var url = "mongodb://localhost:27017/";
 var $ = require('jquery');
 var bodyParser = require('body-parser');
@@ -10,6 +11,10 @@ var express = require('express');
 	app = express();
 
 	app.get ('/', function(req, res){
+
+		var busqueda = req.params;
+		console.log(busqueda);
+			
 		//var dbo = res.db("cocina");
 		//var consulta = dbo.getCollection('recetas').find({});
 		MongoClient.connect(url, function(err, db) {
@@ -18,50 +23,110 @@ var express = require('express');
 
 		  dbo.collection("recetas").find({}).toArray(function(err, result) {
 			  var lista = "";
-		    if (err) throw err;
-				   boton_crear = "<a href=/crear>Añadir</a><br><br>";
-
+			if (err) throw err;
+			
+				   inicio_tabla = ' \
+				   <table class="table"> \
+				   <thead class="thead-dark"> \
+				   <th scope="col">Nombre</th> \
+				   <th scope="col">Duracion</th> \
+				   <th scope="col">Descripcion</th> \
+				   <th scope="col">Tipo</th> \
+				   <th scope="col">Opciones</th> \
+				   </thead> \
+  				   <tbody> \
+				   ';
+					
 				   for (const key in result) {
 
-						li_nombre = "<li>"+result[key]["nombre"]+"</li>";
+						tr_th_nombre = "<tr><th>"+result[key]["nombre"]+"</th>";
+						tr_th_duracion = "<th>"+result[key]["duracion"]+"</th>";
+						tr_th_descripcion = "<th>"+result[key]["descripcion"]+"</th>";
+						tr_th_tipo= "<th>"+result[key]["tipo"]+"</th>";
 
 						id_asociada = result[key]["_id"];
-						boton_editar = "<a href=/editar/"+id_asociada+">Editar</a>\t";
-						boton_borrar = "<a href=/borrar/"+id_asociada+">Borrar</a><br>";
-						lista_sin_sumar = li_nombre+boton_editar+boton_borrar;
+					
+						boton_editar = "<th><a class='btn btn-success btn-lg' href=/editar/"+id_asociada+">Editar</a>\t";
+						boton_borrar = "<a class='btn btn-danger btn-lg' href=/borrar/"+id_asociada+">Borrar</a></th>";
+						
+						fin_tr = "</tr>"
+						
+						lista_sin_sumar = tr_th_nombre+tr_th_duracion+tr_th_descripcion+tr_th_tipo+boton_editar+boton_borrar+fin_tr;
 						lista = lista + lista_sin_sumar;
 				   }
-				   res.send(boton_crear+lista);
-					
-		    	/*
-		    result.forEach(function(receta){
-		    });
-		    */
+				   
+				   cerrar_tabla = '</tbody></table></div>'
+
+				   var buscador_mas_boton_crear = '\
+				       <br><br>\
+					   <div class="container">\
+					   <form action="/">\
+					   <input class="search_input" type="text" name="buscar" placeholder="Search...">\
+					   <input class="btn btn-dark" value="Buscar" type="submit">\
+					   <a class="btn btn-primary" href=/crear>Añadir</a>\
+					   </form>\
+					   <br><br>';
+
+				   lista_entera = buscador_mas_boton_crear + inicio_tabla + lista + cerrar_tabla;					   
+
+				   fs.readFile("head.html","utf8",(err,data)=>{
+					   if(err){
+						   console.log(err);
+						   return err;
+					   }else{
+						   res.send(data+lista_entera);
+					   }
+				   })
+
 		    db.close();
 		  });
 		});	
 	});
-	var formulario = '<form method="POST" action="/crear">\
-						<label for="nombre">Nombre</label>\
-						<input type="text" name="nombre" id="edad">\
-						<br>\
-						<label for="duracion">Duracion</label>\
-						<input type="text" name="duracion">\
-						<br>\
-						<label for="descripcion">Descripcion</label>\
-						<input type="text" name="descripcion">\
-						<br>\
-						<label for="tipo">Tipo</label>\
-						<input type="text" name="tipo">\
-						<br>\
-						<input type="submit">\
+	var formulario = '<form class="form-horizontal" method="POST" action="/crear">\
+						<div class="form-group">\
+						<label class="control-label col-sm-2" for="nombre"><b>Nombre:</b></label>\
+						<div class="col-sm-10">\
+						<input class="form-control" type="text" name="nombre" id="edad">\
+						</div>\
+						</div>\
+						<div class="form-group">\
+						<label class="control-label col-sm-2" for="duracion"><b>Duracion:</b></label>\
+						<div class="col-sm-10">\
+						<input class="form-control" type="text" name="duracion">\
+						</div>\
+						</div>\
+						<div class="form-group">\
+						<label class="control-label col-sm-2" for="descripcion"><b>Descripcion:</b></label>\
+						<div class="col-sm-10">\
+						<input class="form-control" type="text" name="descripcion">\
+						</div>\
+						</div>\
+						<div class="form-group">\
+						<label class="control-label col-sm-2" for="tipo"><b>Tipo:</b></label>\
+						<div class="col-sm-10">\
+						<input class="form-control" type="text" name="tipo">\
+						</div>\
+						</div>\
+						<div class="form-group">\
+						<div class="col-sm-offset-2 col-sm-10">\
+						<a hr\
+						<a class="btn btn-warning" href="/">Volver</a>\
+						<input class="btn btn-dark" type="submit">\
+						</div>\
+						</div>\
 					</form>';
 
 	app.get ('/crear', function(req, res){
 
-	  res.send('<html><body>'
-		  +formulario
-		  + '</body></html>');
+		fs.readFile("head.html","utf8",(err,data)=>{
+			if(err){
+				console.log(err);
+				return err;
+			}else{
+				res.send(data+formulario);
+			}
+		})
+
 	});
 
 	app.post ('/crear',urlencodedParser, function(req, res){
@@ -114,23 +179,47 @@ var express = require('express');
 			var dbo = db.db("cocina");
 			dbo.collection("recetas").findOne({_id: new ObjectID(param_id)}, function(err, resultado) {			
 
-				var formulario_editar = '<form method="POST" action="/editar/'+[param_id]+'">\
-								<label for="nombre">Nombre</label>\
-								<input type="text" name="nombre" id="edad" value="'+resultado["nombre"]+'">\
-								<br>\
-								<label for="duracion">Duracion</label>\
-								<input type="text" name="duracion" value="'+resultado["duracion"]+'">\
-								<br>\
-								<label for="descripcion">Descripcion</label>\
-								<input type="text" name="descripcion" value="'+resultado["descripcion"]+'">\
-								<br>\
-								<label for="tipo">Tipo</label>\
-								<input type="text" name="tipo" value="'+resultado["tipo"]+'">\
-								<br>\
-								<input type="submit">\
+				var formulario_editar = '<form class="form-horizontal" method="POST" action="/editar/'+[param_id]+'">\
+								<div class="form-group">\
+								<label class="control-label col-sm-2" for="nombre"><b>Nombre:</b></label>\
+								<div class="col-sm-10">\
+								<input class="form-control" type="text" name="nombre" id="edad" value="'+resultado["nombre"]+'">\
+								</div>\
+								</div>\
+								<div class="form-group">\
+								<label class="control-label col-sm-2" for="duracion"><b>Duracion:</b></label>\
+								<div class="col-sm-10">\
+								<input class="form-control" type="text" name="duracion" value="'+resultado["duracion"]+'">\
+								</div>\
+								</div>\
+								<div class="form-group">\
+								<label class="control-label col-sm-2" for="descripcion"><b>Descripcion:</b></label>\
+								<div class="col-sm-10">\
+								<input class="form-control" type="text" name="descripcion" value="'+resultado["descripcion"]+'">\
+								</div>\
+								</div>\
+								<div class="form-group">\
+								<label class="control-label col-sm-2" for="tipo"><b>Tipo:</b></label>\
+								<div class="col-sm-10">\
+								<input class="form-control" type="text" name="tipo" value="'+resultado["tipo"]+'">\
+								</div>\
+								</div>\
+								<div class="form-group">\
+								<div class="col-sm-offset-2 col-sm-10">\
+								<a class="btn btn-warning" href="/">Volver</a>\
+								<input class="btn btn-dark" type="submit">\
+								</div>\
+								</div>\
 							</form>';
 
-				res.send(formulario_editar);
+				fs.readFile("head.html","utf8",(err,data)=>{
+					if(err){
+						console.log(err);
+						return err;
+					}else{
+						res.send(data+formulario_editar);
+					}
+				})
 			});
 		});
 	});
